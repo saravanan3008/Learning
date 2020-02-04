a40d537f464166360b2903c2cfb7da3ad8435994
Jeya.George <jeya.george@scouting.org>
2020-02-04 19:39:28 +0530

    switchloop

---
+++ Switchcase.js
@@ -0,0 +1,37 @@
+const  getDay = (value) => {
+    let date = value;
+    const getday = new Date(date);
+    let  days = getday.getDay();
+    
+
+switch(days){
+    case 0:
+    day = "Sunday";
+    break;
+    case 1:
+    day = "MonDay";
+    break;
+    case 2:
+    day = "Tuesday";
+    break;
+    case 3:
+    day = "WEDESDAY";
+    break;
+    case 4:
+    day = "THUSDAY";
+    break;
+    case 5:
+    day = "FRIDAY";
+    break;
+    case 6:
+    day = "Saturaday";
+    break;
+    default:
+        day= "Invalid the day";
+        break;
+
+}
+console.log(days);
+console.log(day);
+}
+exports.switchgetDay = switchgetDay;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var condnandloops = require('./conditionsandloops');
+var switchloop = require('./Switchcase');
 const Dates = require('./date');
 Dates.getDay("03/31/2013");
+switchloop.switchgetDay("03/13/2014");
 //console.log("hello world");
 
 condnandloops.sum(1,2);
