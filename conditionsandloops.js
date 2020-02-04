3371b9c03d4bf0a8f8f44c6bec6e6e4a00b253cd
Jeya.George <jeya.george@scouting.org>
2020-02-04 21:24:15 +0530

    pasue

--- conditionsandloops.js
+++ conditionsandloops.js
@@ -1,11 +1,11 @@
 const  sum = (a,b) =>{
-    return a+b;
+    return a + b;
 }
 const sub = (a,b) => {
-    return a-b;
+    return a - b;
 }
 const mul= (a,b) =>{
-    return a*b;
+    return a * b;
 }
 exports.sum =sum;
 exports.sub =sub;
